4bcd5b472905386ba15b1258c46400bc0d0323d8
Felix Obenhuber <felix.obenhuber@helsing.ai>
2025-12-04 09:47:38 +0100

    Fix fmt

--- src/lossy_lines.rs
+++ src/lossy_lines.rs
@@ -162,9 +162,7 @@ impl Decoder for LossyLinesCodec {
                     // newline, return an error and start discarding on the
                     // next call.
                     self.is_discarding = true;
-                    Err(io::Error::other(
-                        "line length limit exceeded",
-                    ))
+                    Err(io::Error::other("line length limit exceeded"))
                 } else {
                     // We didn't find a line or reach the length limit, so the next
                     // call will resume searching at the current offset.
